22e737af7c8375cec272b892646be836805ec8a7
hazecodeio <3461124+hazecodeio@users.noreply.github.com>
2024-08-05 20:25:49 -0500

    committing changes..

--- scripts-of-bash/scala2_12-remove.sh
+++ scripts-of-bash/scala2_12-remove.sh
@@ -10,4 +10,4 @@ suffix=2_12
 sdk_home=/opt/langs/scala/scala2_12-current/
 sdk_bin=$sdk_home/bin
 
-find $sdk_bin -type f -regextype posix-extended -regex '(./|/).*' | grep --color -iE '[a-Z]+\.[a-Z]+$' -v | grep --color -iE '[a-Z]+$' -o | xargs echo -n | jq --arg suffix $suffix -R 'split(" ") | .[]' | xargs -i update-alternatives --remove-all {}$suffix
+find $sdk_bin -type f -regextype posix-extended -regex '(./|/).*' | grep --color -iE '[a-Z]+\.[a-Z]+$' -v | grep --color -iE '[a-Z]+$' -o | xargs echo -n | jq --arg suffix $suffix -R 'split(" ") | .[]' | sudo -- xargs -i update-alternatives --remove-all {}$suffix
--- scripts-of-bash/scala2_13-remove.sh
+++ scripts-of-bash/scala2_13-remove.sh
@@ -10,4 +10,4 @@ suffix=2_13
 sdk_home=/opt/langs/scala/scala2_13-current
 sdk_bin=$sdk_home/bin
 
-find $sdk_bin -type f -regextype posix-extended -regex '(./|/).*' | grep --color -iE '[a-Z]+\.[a-Z]+$' -v | grep --color -iE '[a-Z]+$' -o | xargs echo -n | jq --arg suffix $suffix -R 'split(" ") | .[]' | xargs -i update-alternatives --remove-all {}$suffix
+find $sdk_bin -type f -regextype posix-extended -regex '(./|/).*' | grep --color -iE '[a-Z]+\.[a-Z]+$' -v | grep --color -iE '[a-Z]+$' -o | xargs echo -n | jq --arg suffix $suffix -R 'split(" ") | .[]' | sudo -- xargs -i update-alternatives --remove-all {}$suffix
