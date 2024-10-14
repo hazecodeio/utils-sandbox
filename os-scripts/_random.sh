f9d61334ed5c5b7f100d009004deb9ad463797f3
hazecodeio <3461124+hazecodeio@users.noreply.github.com>
2024-10-14 08:18:47 -0500

    committing changes..

--- os-scripts/_random.sh
+++ os-scripts/_random.sh
@@ -1,3 +1,4 @@
+# Extract credentials for a pinned account
 cat vault.json | jq '.vaults
       | to_entries
       | .[].value
@@ -9,3 +10,21 @@ cat vault.json | jq '.vaults
       | select(.itemUsername
       | contains("USERNAME"))
       | .password'
+
+
+
+# Deny IPv6 for an app
+sudo addgroup no-ipv6
+
+sudo ip6tables --list
+sudo ip6tables -A OUTPUT -m owner --gid-owner no-ipv6 -j DROP
+sudo ip6tables --list
+
+sudo -g no-ipv6 _COMMAND_
+
+## Persist ip6tables changes
+sudo ip6tables-save
+sudo ip6tables-restore
+
+## You can easily write a wrapper script that uses sudo for you. You can get rid of the password prompt by adding
+sudo     ALL=(:no-internet)      NOPASSWD: ALL
