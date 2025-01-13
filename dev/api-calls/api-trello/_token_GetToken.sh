f838322a8ba63f3fed09046f0be7e489d164d25c
hazecodeio <3461124+hazecodeio@users.noreply.github.com>
2025-01-12 20:11:45 -0600

    committing changes..

--- dev/api-calls/api-trello/_token_GetToken.sh
+++ dev/api-calls/api-trello/_token_GetToken.sh
@@ -3,5 +3,5 @@ CWD=$(echo $(realpath "${0}") | xargs dirname)
 source "${CWD}"/_env-loader.sh
 
 curl  -H "Authorization: OAuth oauth_consumer_key=\"${TRELLO_KEY}\", oauth_token=\"${TRELLO_TOKEN}\"" \
-      --URL  "https://api.trello.com/1/members/me" \
+      --URL  "https://api.trello.com/1/tokens/${TRELLO_TOKEN}" \
       | jq
