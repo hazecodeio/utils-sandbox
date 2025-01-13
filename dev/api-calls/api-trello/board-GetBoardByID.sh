5d81cc8312919ddf568727e9335b45558d777451
hazecodeio <3461124+hazecodeio@users.noreply.github.com>
2025-01-12 20:32:30 -0600

    committing changes..

--- dev/api-calls/api-trello/board-GetBoardByID.sh
+++ dev/api-calls/api-trello/board-GetBoardByID.sh
@@ -3,7 +3,7 @@ CWD=$(echo $(realpath "${0}") | xargs dirname)
 source "${CWD}"/_env-loader.sh
 
 SHORT_LINK=xZEcS7Kw
-#BOARD_ID=5b0c36886df89b91a195f6cb
+BOARD_ID=5b0c36886df89b91a195f6cb
 
 curl  -H "Authorization: OAuth oauth_consumer_key=\"${TRELLO_KEY}\", oauth_token=\"${TRELLO_TOKEN}\"" \
       -H "Accept: application/json" \
