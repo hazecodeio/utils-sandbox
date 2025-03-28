f4f4fa218de6f53b33cc79a60c8c20efcbaaef5a
hazecodeio <3461124+hazecodeio@users.noreply.github.com>
2025-03-28 11:25:33 -0500

    committing changes...

--- api-calls/api-trello/_env-loader.sh
+++ api-calls/api-trello/_env-loader.sh
@@ -8,7 +8,7 @@ function funinit() {
   #echo "${CWD}"
 
   #find ~/dev/api-calls-sandbox -type f -regex '.*.properties'| xargs cat
-  eval export "$(find "${CWD}" -type f -regextype posix-extended -regex "^(../|./|/).*(_env-)*.properties$" | xargs cat)"
+  eval export "$(find "${CWD}" -type f -regextype posix-extended -regex "^(../|./|/).*(_env-)(\w+).properties$" | xargs cat)"
 }
 
 funinit
